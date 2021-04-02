c9883bf09ef9cea2ac09b3a6992d8da15c2ea1e8
galvezmjoshua <56462373+galvezmjoshua@users.noreply.github.com>
2021-04-02 13:45:56 -0400

    Update line.js

--- models/line.js
+++ models/line.js
@@ -6,20 +6,16 @@ const lineSchema = new mongoose.Schema({
     // field1: task
     type: String, // task is a string
     unique: true, // it has to be unique
-    required: true, // it is required
   },
   Items: {
     // field2: completed
     type: Array, // it is a boolean
-    required: true, // it is required
   },
   Type : {
     type: String,
-    required: true,
   },
   Start: {
     type: Date,
-    required: false,
   },
 })
 
